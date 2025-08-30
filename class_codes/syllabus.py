52510eb9cd642be7f6d8d4c4b3a9dd8bec8a7f05
Arun CS <aruncs31ss@gmail.com>
2025-08-30 21:49:54 +0530

    update

---
+++ class_codes/syllabus.py
@@ -0,0 +1,8 @@
+# Basic coding skills 
+# Working with data types, Keywords, Variables and Operators, 
+# working with numeric data, Type conversions, Input, Processing, and 
+# Output, Formatting output. 
+# Data Types - String, Numeric(int, float, complex), Bool, List, Tuple, 
+# Dict, Set and Frozen Set 
+# Control statements - Selection structure (if-else), Iteration structure 
+# (for, while)
